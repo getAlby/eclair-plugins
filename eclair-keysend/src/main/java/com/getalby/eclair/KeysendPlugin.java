58aafd51ceff9d8d1a4e13a26cfa7f040309252c
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2023-05-06 14:05:29 +0200

    Initial working version of docker build with two plugins for eclair

--- eclair-keysend/src/main/java/com/getalby/eclair/KeysendPlugin.java
+++ eclair-keysend/src/main/java/com/getalby/eclair/KeysendPlugin.java
@@ -7,40 +7,25 @@ import akka.http.scaladsl.server.RouteResult;
 import fr.acinq.bitcoin.PublicKey;
 import fr.acinq.bitcoin.scalacompat.ByteVector32;
 import fr.acinq.bitcoin.scalacompat.Crypto;
-import fr.acinq.eclair.CltvExpiryDelta;
-import fr.acinq.eclair.Kit;
-import fr.acinq.eclair.MilliSatoshi;
-import fr.acinq.eclair.Plugin;
-import fr.acinq.eclair.PluginParams;
-import fr.acinq.eclair.RouteProvider;
-import fr.acinq.eclair.Setup;
+import fr.acinq.eclair.*;
 import fr.acinq.eclair.api.directives.EclairDirectives;
 import fr.acinq.eclair.payment.relay.Relayer;
 import fr.acinq.eclair.payment.send.PaymentInitiator;
 import fr.acinq.eclair.router.Graph;
-import fr.acinq.eclair.router.Router;
 import fr.acinq.eclair.router.Graph.HeuristicsConstants;
 import fr.acinq.eclair.router.Graph.WeightRatios;
+import fr.acinq.eclair.router.Router;
 import fr.acinq.eclair.wire.protocol.GenericTlv;
 import scala.Function1;
 import scala.Option;
-import scala.collection.JavaConverters;
-import scala.collection.Seq;
-import scala.collection.immutable.Nil;
-import scala.collection.mutable.Buffer;
+import scala.collection.immutable.Set;
+import scala.compat.java8.FutureConverters;
 import scala.concurrent.Future;
 import scala.util.Left;
-import scala.compat.java8.FutureConverters;
+import scodec.bits.ByteVector;
 
-
-import static akka.http.javadsl.server.Directives.onComplete;
-import static akka.http.javadsl.server.Directives.path;
-import static akka.http.javadsl.server.Directives.complete;
-
+import static akka.http.javadsl.server.Directives.*;
 import static akka.pattern.Patterns.ask;
-
-import java.util.List;
-import java.util.UUID;
 
 public class KeysendPlugin implements Plugin, RouteProvider {
 
@@ -73,21 +58,18 @@ public class KeysendPlugin implements Plugin, RouteProvider {
                 heuristics,
                 mpp, "", false);
         PaymentInitiator.SendSpontaneousPayment ssp = new PaymentInitiator.SendSpontaneousPayment(
-            new MilliSatoshi(1000),
-            new Crypto.PublicKey(PublicKey.fromHex("031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f")),
-            ByteVector32.One(),
-            1,
-            null,
-            params,
-            null,
-            false);
+                new MilliSatoshi(1000),
+                new Crypto.PublicKey(PublicKey.fromHex("037e9d070515c8ba32e6a32fa698e568fc4944a1bb67ae2048947511267202509b")),
+                ByteVector32.One(),
+                1,
+                Option.apply(""),
+                params,
+                new Set.Set1<GenericTlv>(new GenericTlv(UInt64.apply(1), ByteVector.empty())).toSeq(),
+                false);
         final var fut = ask(kit.paymentInitiator(), ssp, 1000L);
         final var s = FutureConverters.toJava(fut);
         return path("keysend", () ->
                 onComplete(s, res -> complete((String) res.get().toString()))).asScala();
 
-    }
-    public Seq<GenericTlv> convertListToSeq(List<GenericTlv> inputList) {
-        return JavaConverters.asScalaIteratorConverter(inputList.iterator()).asScala().toSeq();
     }
 }
